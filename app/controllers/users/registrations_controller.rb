639625bcc2526abc6391df2af9804fd630369250
Jaden Slagle <jaden.slagle@gmail.com>
2016-02-14 05:19:44 +0000

    Closed spacing in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
   def create
     super do |resource|
       if params[:plan]
@@ -12,5 +11,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
-  
 end
