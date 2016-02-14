634afc2e951bfbda6d13d13ce5e30232472cad3b
Jaden Slagle <jaden.slagle@gmail.com>
2016-02-14 19:52:42 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
+  
   def save_with_payment
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
