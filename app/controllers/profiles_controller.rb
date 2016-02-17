b39d06f6b50e279b31c23698a39930b567ee0fd9
Jaden Slagle <jaden.slagle@gmail.com>
2016-02-17 09:37:29 +0000

    Added users show action and fixed type on profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,7 +6,7 @@ class ProfilesController < ApplicationController
   end
   
   def create
-    @user = User.find( params[user_id] )
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile Created!"
